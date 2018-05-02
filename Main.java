f470253ca864b27a363dd153343327857d536923
Henry T <henryt.inc@gmail.com>
2018-05-01 21:30:33 -0400

    Some java magic

--- Main.java
+++ Main.java
@@ -1,8 +1,52 @@
 // Main.java
 
-public class Main {
+import java.awt.*;
+import java.awt.event.*;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.swing.*;
+import java.util.Random;
 
-    public static void main(String[] args) {
+public class Main extends JFrame implements ActionListener, ComponentListener {
+
+    public static int w = 800;
+    public static int h = 600;
+
+    public Main() {
+        super("[RASTERA] HUBG");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        setSize(w, h);
+        setMinimumSize(new Dimension(800, 600));
+        setLayout(new BorderLayout());
 
     }
+
+    public void componentHidden(ComponentEvent ce) {
+
+    }
+
+    public void componentShown(ComponentEvent ce) {
+
+    }
+
+    public void componentMoved(ComponentEvent ce) {
+
+    }
+
+    public void componentResized(ComponentEvent ce) {
+        w = getWidth();
+        h = getHeight();
+    }
+
+    public void actionPerformed(ActionEvent evt) {
+
+    }
+
+
+    public static void main(String[] args) {
+        Main frame = new Main();
+    }
+
 }
