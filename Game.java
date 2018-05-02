d8f8ffdd08d97fb27f8e0cd7f103711a0af5e777
Henry T <henryt.inc@gmail.com>
2018-05-02 14:39:11 -0400

    Some java magic

---
+++ Game.java
@@ -0,0 +1,27 @@
+// Some game magic bs
+
+import javax.imageio.ImageIO;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class Game extends JFrame implements KeyListener {
+
+    private Main parent;
+
+    public Game(Main parent) {
+        this.parent = parent;
+
+    }
+
+}
