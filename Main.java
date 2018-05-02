e68511021ec7ffed4178ee0d8dca076b138c936f
Henry T <henryt.inc@gmail.com>
2018-05-01 20:36:36 -0400

    Init

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
-// Gei.js
+// Main.java
 
 public class Main {
+
     public static void main(String[] args) {
 
     }
--- server/Main.java
+++
@@ -1,7 +0,0 @@
-// Gei.js
-
-public class Main {
-    public static void main(String[] args) {
-
-    }
-}
