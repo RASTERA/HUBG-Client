1e1d04e7f36db21623c48aef135409259094364f
Syed <syed.safwaan@gmail.com>
2018-05-27 16:44:34 -0400

    Adjusted component alignment

--- Login.java
+++ Login.java
@@ -21,251 +21,269 @@ import java.util.ArrayList;
 
 class Login extends GeiPanel implements ActionListener, KeyListener, MouseListener {
 
-    private final GeiTextField emailField;
-    private final JPasswordField passwordField;
-    private final JLabel emailLabel;
-    private final JLabel passwordLabel;
-    private final JLabel forgetPasswordLabel;
-    private final JLabel createAccountLabel;
-    private final GeiButton loginButton;
-
-    private final ArrayList<BufferedImage> backgroundFrames = new ArrayList<>();
-    private BufferedImage rasteraLogo;
-    private BufferedImage hubgLogo;
-
-    private int frame = 0;
-    private final int frameCap = 30;
-
-    public Login(Main parent) {
-        this.parent = parent;
-        this.parent.setMasterTimer(45);
-
-        //this.constantUpdate = false;
-
-        this.addKeyListener(this);
-        this.setFocusable(true);
-        this.setLayout(null);
-
-        try {
-            String fileName;
-
-            for (int i = 0; i <= this.frameCap; i++) {
-                fileName = String.format("images/menu_animation/%d.png", i);
-                this.backgroundFrames.add(ImageIO.read(new File(fileName)));
-            }
-
-            this.rasteraLogo = ImageIO.read(new File("images/rastera.png"));
-            this.hubgLogo = ImageIO.read(new File("images/hubg-logo.png"));
-
-            AudioInputStream audioIn = AudioSystem.getAudioInputStream(this.getClass().getResource("music/menu.wav"));
-            Clip clip = AudioSystem.getClip();
-            clip.open(audioIn);
-            clip.start();
-            clip.loop(clip.LOOP_CONTINUOUSLY);
-        } catch (Exception e) {
-            Main.errorQuit(e);
-        }
-
-        this.emailField = new GeiTextField();
-        this.passwordField = new JPasswordField();
-
-        this.emailLabel = new JLabel("Email/Username");
-        this.passwordLabel = new JLabel("Password");
-        this.forgetPasswordLabel = new JLabel("Forgot your password?");
-        this.createAccountLabel = new JLabel("Create an account");
-
-        this.emailLabel.setForeground(Color.WHITE);
-        this.passwordLabel.setForeground(Color.WHITE);
-
-        this.loginButton = new GeiButton("SIGN IN");
-        this.loginButton.setActionCommand("login");
-        this.loginButton.addActionListener(this);
-        this.loginButton.setEnabled(false);
-
-        // Submit on enter
-        this.emailField.addActionListener(e -> this.loginButton.doClick());
-
-        this.passwordField.addActionListener(e -> this.loginButton.doClick());
-
-        // Validate text
-        this.emailField.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-        this.emailField.addKeyListener(new KeyAdapter() {
-            public void keyReleased(KeyEvent e) {
-                super.keyReleased(e);
+	private final GeiTextField emailField;
+	private final JPasswordField passwordField;
+	private final JLabel emailOrUserLabel;
+	private final JLabel passwordLabel;
+	private final JLabel forgetPasswordLabel;
+	private final JLabel createAccountLabel;
+	private final GeiButton loginButton;
+
+	private final ArrayList<BufferedImage> backgroundFrames = new ArrayList<>();
+	private BufferedImage rasteraLogo;
+	private BufferedImage hubgLogo;
+
+	private int frame = 0;
+	private final int frameCap = 30;
+
+	public Login(Main parent) {
+		this.parent = parent;
+		this.parent.setMasterTimer(45);
+
+		//this.constantUpdate = false;
+
+		this.addKeyListener(this);
+		this.setFocusable(true);
+		this.setLayout(null);
 
-                if (e.getKeyCode() != KeyEvent.VK_ENTER) {
-                    Login.this.validateText();
-                }
-            }
-        });
+		try {
+			String fileName;
+
+			for (int i = 0; i <= this.frameCap; i++) {
+				fileName = String.format("images/menu_animation/%d.png", i);
+				this.backgroundFrames.add(ImageIO.read(new File(fileName)));
+			}
+
+			this.rasteraLogo = ImageIO.read(new File("images/rastera.png"));
+			this.hubgLogo = ImageIO.read(new File("images/hubg-logo.png"));
+
+			AudioInputStream audioIn = AudioSystem.getAudioInputStream(this.getClass().getResource("music/menu.wav"));
+			Clip clip = AudioSystem.getClip();
+			clip.open(audioIn);
+			clip.start();
+			clip.loop(clip.LOOP_CONTINUOUSLY);
+		} catch (Exception e) {
+			Main.errorQuit(e);
+		}
+
+		this.emailField = new GeiTextField();
+		this.passwordField = new JPasswordField();
+
+		this.emailOrUserLabel = new JLabel("Email/Username");
+		this.passwordLabel = new JLabel("Password");
+		this.forgetPasswordLabel = new JLabel("Forgot your password?");
+		this.createAccountLabel = new JLabel("Create an account");
+
+		this.emailOrUserLabel.setForeground(Color.WHITE);
+		this.passwordLabel.setForeground(Color.WHITE);
+
+		this.loginButton = new GeiButton("SIGN IN");
+		this.loginButton.setActionCommand("login");
+		this.loginButton.addActionListener(this);
+		this.loginButton.setEnabled(false);
+
+		// Submit on enter
+		this.emailField.addActionListener(e -> this.loginButton.doClick());
+
+		this.passwordField.addActionListener(e -> this.loginButton.doClick());
+
+		// Validate text
+		this.emailField.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		this.emailField.addKeyListener(new KeyAdapter() {
+
+			@Override
+			public void keyReleased(KeyEvent e) {
+				super.keyReleased(e);
+
+				if (e.getKeyCode() != KeyEvent.VK_ENTER) {
+					Login.this.validateText();
+				}
+			}
+		});
 
-        this.passwordField.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-        this.passwordField.addKeyListener(new KeyAdapter() {
-            public void keyReleased(KeyEvent e) {
-                super.keyReleased(e);
+		this.passwordField.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		this.passwordField.addKeyListener(new KeyAdapter() {
 
-                if (e.getKeyCode() != KeyEvent.VK_ENTER) {
-                    Login.this.validateText();
-                }
-            }
-        });
+			@Override
+			public void keyReleased(KeyEvent e) {
+				super.keyReleased(e);
 
-
-        this.forgetPasswordLabel.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-        this.forgetPasswordLabel.addMouseListener(new MouseAdapter() {
-            public void mouseClicked(MouseEvent e) {
-                Rah.webbrowserOpen("https://rastera.xyz");
-            }
-        });
+				if (e.getKeyCode() != KeyEvent.VK_ENTER) {
+					Login.this.validateText();
+				}
+			}
+		});
 
-        this.createAccountLabel.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-        this.createAccountLabel.addMouseListener(new MouseAdapter() {
-            public void mouseClicked(MouseEvent e) {
-                Rah.webbrowserOpen("https://rastera.xyz");
-            }
-        });
 
-        this.add(this.emailLabel);
-        this.add(this.passwordLabel);
-        this.add(this.forgetPasswordLabel);
-        this.add(this.createAccountLabel);
+		this.forgetPasswordLabel.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		this.forgetPasswordLabel.addMouseListener(new MouseAdapter() {
 
-        this.add(this.emailField);
-        this.add(this.passwordField);
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				Rah.webbrowserOpen("https://rastera.xyz");
+			}
+		});
 
-        this.add(this.loginButton);
+		this.createAccountLabel.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		this.createAccountLabel.addMouseListener(new MouseAdapter() {
 
-    }
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				Rah.webbrowserOpen("https://rastera.xyz");
+			}
+		});
 
-    private void validateText() {
-        if (this.emailField.getText().length() > 0 && String.valueOf(this.passwordField.getPassword()).length() > 0) {
-            this.loginButton.setEnabled(true);
-        } else {
-            this.loginButton.setEnabled(false);
-        }
-    }
+		this.add(this.emailOrUserLabel);
+		this.add(this.passwordLabel);
+		this.add(this.forgetPasswordLabel);
+		this.add(this.createAccountLabel);
 
-    private void disableLogin() {
-        this.loginButton.setEnabled(false);
-        this.passwordField.setEditable(false);
-        this.emailField.setEditable(false);
-        this.loginButton.setText("Authenticating...");
-    }
+		this.add(this.emailField);
+		this.add(this.passwordField);
 
-    private void enableLogin() {
+		this.add(this.loginButton);
 
-        this.passwordField.setEditable(true);
-        this.emailField.setEditable(true);
-        this.passwordField.setText("");
-        this.loginButton.setText("SIGN IN");
-    }
+	}
 
-    private void startMenu(Session session) {
-        System.out.println("Login Successful");
-        Main.session = session;
-        this.removeKeyListener(this);
-        this.parent.startPage(Main.Pages.MENU);
-    }
+	private void validateText() {
+		if (this.emailField.getText().length() > 0 && String.valueOf(this.passwordField.getPassword()).length() > 0) {
+			this.loginButton.setEnabled(true);
+		} else {
+			this.loginButton.setEnabled(false);
+		}
+	}
 
-    public void mouseExited(MouseEvent e) {
+	private void disableLogin() {
+		this.loginButton.setEnabled(false);
+		this.passwordField.setEditable(false);
+		this.emailField.setEditable(false);
+		this.loginButton.setText("Authenticating...");
+	}
 
-    }
+	private void enableLogin() {
 
-    public void mouseEntered(MouseEvent e) {
+		this.passwordField.setEditable(true);
+		this.emailField.setEditable(true);
+		this.passwordField.setText("");
+		this.loginButton.setText("SIGN IN");
+	}
 
-    }
+	private void startMenu(Session session) {
+		System.out.println("Login Successful");
+		Main.session = session;
+		this.removeKeyListener(this);
+		this.parent.startPage(Main.Pages.MENU);
+	}
 
-    public void mouseReleased(MouseEvent e) {
+	@Override
+	public void mouseExited(MouseEvent e) {
 
-    }
+	}
 
-    public void mousePressed(MouseEvent e) {
+	@Override
+	public void mouseEntered(MouseEvent e) {
 
-    }
+	}
 
-    public void mouseClicked(MouseEvent e) {
+	@Override
+	public void mouseReleased(MouseEvent e) {
 
-    }
+	}
 
-    public void actionPerformed(ActionEvent e) {
-        switch (e.getActionCommand()) {
-            case "login":
+	@Override
+	public void mousePressed(MouseEvent e) {
 
-                this.disableLogin();
+	}
 
-                System.out.println("Trying to login...");
+	@Override
+	public void mouseClicked(MouseEvent e) {
 
-                // Separate thread so that UI thread is not blocked
-                Thread authentication = new Thread(() -> {
+	}
 
-                    //Session session = new Session("Hi", "Hi");
-                    Session session = Communicator.login(Login.this.emailField.getText(), String.valueOf(Login.this.passwordField.getPassword()));
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		switch (e.getActionCommand()) {
+			case "login":
 
-                    if (session == null) {
-                        JOptionPane.showMessageDialog(null, "An error occurred while connecting to the authentication server. Please try again later.", "RASTERA Authentication Service", JOptionPane.ERROR_MESSAGE);
-                        Login.this.enableLogin();
-                    } else if (session.getUsername().equals("")) {
-                        JOptionPane.showMessageDialog(null, session.getToken(), "RASTERA Authentication Service", JOptionPane.ERROR_MESSAGE);
-                        Login.this.enableLogin();
-                    } else {
-                        Login.this.startMenu(session);
-                    }
+				this.disableLogin();
 
+				System.out.println("Trying to login...");
 
-                });
+				// Separate thread so that UI thread is not blocked
+				Thread authentication = new Thread(() -> {
 
-                authentication.start();
-                break;
-        }
-    }
+					//Session session = new Session("Hi", "Hi");
+					Session session = Communicator.login(Login.this.emailField.getText(), String.valueOf(Login.this.passwordField.getPassword()));
 
-    public void keyTyped(KeyEvent e) {
-    }
+					if (session == null) {
+						JOptionPane.showMessageDialog(null, "An error occurred while connecting to the authentication server. Please try again later.", "RASTERA Authentication Service", JOptionPane.ERROR_MESSAGE);
+						Login.this.enableLogin();
+					} else if (session.getUsername().equals("")) {
+						JOptionPane.showMessageDialog(null, session.getToken(), "RASTERA Authentication Service", JOptionPane.ERROR_MESSAGE);
+						Login.this.enableLogin();
+					} else {
+						Login.this.startMenu(session);
+					}
 
-    public void keyPressed(KeyEvent e) {
 
-    }
+				});
 
-    public void keyReleased(KeyEvent e) {
+				authentication.start();
+				break;
+		}
+	}
 
-    }
+	@Override
+	public void keyTyped(KeyEvent e) {
+	}
 
-    public void paintComponent(Graphics g) {
+	@Override
+	public void keyPressed(KeyEvent e) {
 
-        this.parent.updateFrameRate();
+	}
 
-        this.emailLabel.setBounds(Main.w - 230, 140, 210, 20);
-        this.emailField.setBounds(Main.w - 230, 160, 210, 30);
+	@Override
+	public void keyReleased(KeyEvent e) {
 
-        this.passwordLabel.setBounds(Main.w - 230, 200, 210, 20);
-        this.passwordField.setBounds(Main.w - 230, 220, 210, 30);
+	}
 
-        this.loginButton.setBounds(Main.w - 230, Main.h - 200, 210, 30);
+	@Override
+	public void paintComponent(Graphics g) {
 
-        this.forgetPasswordLabel.setBounds(Main.w - 230, Main.h - 70, 210, 20);
-        this.createAccountLabel.setBounds(Main.w - 230, Main.h - 90, 210, 20);
+		this.parent.updateFrameRate();
 
-        g.setColor(Color.WHITE);
-        g.fillRect(0, 0, Main.w, Main.h);
+		this.emailOrUserLabel.setBounds(Main.w - 238, 140, 210, 20);
+		this.emailField.setBounds(Main.w - 238, 160, 210, 30);
 
-        int size = Math.max(Main.w - 250, Main.h);
+		this.passwordLabel.setBounds(Main.w - 238, 200, 210, 20);
+		this.passwordField.setBounds(Main.w - 238, 220, 210, 30);
 
-        g.drawImage(this.backgroundFrames.get(this.frame), 0, 0, size, size, this);
-        g.drawImage(this.rasteraLogo, 30, this.getHeight() - 55, 150, 25, this);
+		this.loginButton.setBounds(Main.w - 238, Main.h - 200, 210, 30);
 
+		this.forgetPasswordLabel.setBounds(Main.w - 238, Main.h - 70, 210, 20);
+		this.createAccountLabel.setBounds(Main.w - 238, Main.h - 90, 210, 20);
 
-        g.setColor(new Color(1, 10, 19));
-        g.fillRect(Main.w - 250, 0, 250, Main.h);
+		g.setColor(Color.WHITE);
+		g.fillRect(0, 0, Main.w, Main.h);
 
-        g.drawImage(this.hubgLogo, Main.w - 230, 40, 210, 66, this);
+		int size = Math.max(Main.w - 250, Main.h);
 
-        if (this.frame == this.frameCap) {
-            this.frame = 0;
-        } else {
-            this.frame++;
-        }
+		g.drawImage(this.backgroundFrames.get(this.frame), 0, 0, size, size, this);
+		g.drawImage(this.rasteraLogo, 30, this.getHeight() - 55, 150, 25, this);
 
-    }
+
+		g.setColor(new Color(1, 10, 19));
+		g.fillRect(Main.w - 250, 0, 250, Main.h);
+
+		g.drawImage(this.hubgLogo, Main.w - 238, 40, 210, 66, this);
+
+		if (this.frame == this.frameCap) {
+			this.frame = 0;
+		} else {
+			this.frame++;
+		}
+
+	}
 
 
 }
