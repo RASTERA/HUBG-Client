1025bbe456caa72836c6b667123ae52f0d074cf8
Henry T <henryt.inc@gmail.com>
2018-06-03 14:49:25 -0400

    Merge Assets

---
+++ assets/TestMap.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="TestMap" tilewidth="200" tileheight="200" tilecount="4" columns="2">
+ <image source="TestMap.png" width="400" height="400"/>
+</tileset>
---
+++ assets/tileset_gutter.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tileset_gutter" tilewidth="16" tileheight="16" spacing="2" margin="1" tilecount="924" columns="33">
+ <image source="tileset_gutter.png" width="594" height="504"/>
+</tileset>
